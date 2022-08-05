d79ceabeaf685da90dce64fb92390bfcfd36e3f0
taisei <taisei1242.tk78@gmail.com>
2022-08-05 16:50:51 +0900

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { RecoilRoot } from "recoil"; //追加
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 
-export default MyApp
+export default MyApp;
